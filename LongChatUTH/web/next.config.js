0c3e1625afedf280b57a9e79153a978a963c4e65
congtai308@gmail.com <Congtai308>
2026-06-24 04:41:45 +0700

    up

--- LongChatUTH/web/next.config.js
+++ LongChatUTH/web/next.config.js
@@ -4,8 +4,9 @@ const nextConfig = {
     unoptimized: true,
     remotePatterns: [
       { protocol: "http", hostname: "localhost" },
-      { protocol: "https", hostname: "**.nhathuoclongchau.com.vn" }
-    ]
-  }
+      { protocol: "https", hostname: "**.nhathuoclongchau.com.vn" },
+      { protocol: "https", hostname: "nhathuoctaman.freedev.app" },
+    ],
+  },
 };
 module.exports = nextConfig;
